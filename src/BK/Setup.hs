c5d34adc00f705adaf2a4ba2b4f9afa166149664
Harley D. Eades III <harley.eades@pm.me>
2026-04-16 14:08:40 -0400

    wip on Setup
    Getting close to shell completion setup.

--- src/BK/Setup.hs
+++ src/BK/Setup.hs
@@ -7,6 +7,7 @@ module BK.Setup
 -- | * External Imports
 
 import Data.Text              (Text)
+import Control.Monad          (when)
 import Control.Monad.IO.Class (MonadIO(..))
 import System.FilePath        ((</>))
 import System.Exit            (exitFailure)
@@ -18,7 +19,6 @@ import qualified System.Console.Haskeline as HL
 -- | * Internal Imports
 import BK.BKMap qualified as BK
 import BK.Lib   qualified as Lib
-import Control.Monad (when)
 
 -- | * Setup
 
@@ -31,7 +31,7 @@ _debugSetup = False
 -- then we enter an interactive setup process. 
 -- Nonrecoverable errors exit with failure.
 setup :: IO BKConfigPaths
-setup = do
+setup = 
   -- Get $BK_CONFIG_DIR from the environment, if it isn't set, then continue
   -- with $HOME as the config path.
   Env.lookupEnv "BK_CONFIG_DIR" 
@@ -141,6 +141,35 @@ promptForFilePath prefix defaultValue = _promptForFilePath
                         else do liftIO $ Lib.putStrLnStdOut "\nPlease choose a directory that exists."
                                 _promptForFilePath)
 
+squareBracket :: Text -> Text 
+squareBracket t = "["<>t<>"]"
+
+questionMark :: Text
+questionMark = "?"
+
+promptForShell :: Text -> HL.InputT IO Shell
+promptForShell prefix = do
+        osShellM <- liftIO $ Env.lookupEnv "SHELL" 
+        let defaultValue = DT.pack $ maybe "" id osShellM
+        liftIO $ when (DT.null defaultValue) $ exitFailureWithMsg "Aborting setup!\nUnable to determine shell"
+        let prompt = DT.unpack $ prefix <> space <> squareBracket "bzf" <> questionMark <> space
+        _promptForShell prompt defaultValue
+  where        
+    _promptForShell prompt defaultValue = do
+        liftIO $ Lib.putStrLnStdOut $ 
+                 newline 
+               <> "Shell configuration"<>newline
+               <> "Supported shells: [b]ash, [z]sh, and [f]ish" <> newline
+               <> "Your current shell is "<>defaultValue
+        HL.getInputLine prompt
+          >>= maybe (_promptForShell prompt defaultValue)
+                (\sh -> case sh of
+                          "" -> _
+                          "b" -> return Bash
+                          "z" -> return Zsh
+                          "f" -> return Fish
+                          _ -> _promptForShell prompt defaultValue)
+
 -- | Interactive setup that configures @bk@ for the first time.
 interactiveSetup :: IO BKConfigPaths
 interactiveSetup = HL.runInputT HL.defaultSettings $ loop 
@@ -154,8 +183,12 @@ interactiveSetup = HL.runInputT HL.defaultSettings $ loop
              configDirParent <- promptForFilePath 
                                   "Enter the directory where you would like to store bk's configuration directory" 
                                   homedir
-             let configFilePaths = bkConfigPaths $ configDirParent </> "bk"
-             liftIO $ initializeWorkDir configFilePaths
+             let configFilePaths = bkConfigPaths $ configDirParent 
+                               </> if configDirParent == homedir
+                                   then ".bk"
+                                   else "bk"
+             shell <- promptForShell "Which shell should be initialized"
+             liftIO $ initializeConfigDir configFilePaths shell
              return configFilePaths
   
     secondRunMessage :: Text
@@ -165,14 +198,87 @@ interactiveSetup = HL.runInputT HL.defaultSettings $ loop
       <> "longer read the environment variable $BK_CONFIG_DIR.\n\n"
       <> "Please check that your shells configuration is exporting $BK_CONFIG_DIR"
 
-initializeWorkDir :: BKConfigPaths -> IO ()
-initializeWorkDir configFilesPaths = do 
+initializeConfigDir :: BKConfigPaths -> Shell -> IO ()
+initializeConfigDir configFilesPaths sh = do 
     let configDir = bkConfigDirectoryPath configFilesPaths
     let csvFile   = bkBookmarksFile       configFilesPaths
-    Lib.createDirectoryIfMissing False configDir    
     b <- Lib.doesFileExist csvFile
     when b $ do exitFailureWithMsg $ "Aborting setup!\n"
                                   <> "It seems bk is already setup, because " 
                                   <> DT.pack csvFile
                                   <> " already exists."
+    Lib.createDirectoryIfMissing False configDir
+    createCompletionsDir sh 
     BK.writeCSVFile csvFile BK.emptyBKMap
+  where
+    createCompletionsDir Fish = return ()
+    createCompletionsDir _sh = do
+      homedir <- Lib.getHomeDirectory
+      let path = completionsDirPath homedir configFilesPaths _sh
+      Lib.createDirectoryIfMissing False path
+
+-- | Supported shell initialization.
+data Shell = Zsh 
+           | Bash 
+           | Fish
+
+doubleQuote :: Text -> Text
+doubleQuote t = "\""<>t<>"\""
+
+singleQuote :: Text -> Text
+singleQuote t = "'"<>t<>"'"
+
+space :: Text 
+space = " "
+
+colon :: Text
+colon = ":"
+
+newline :: Text
+newline = "\n"
+
+exportEnvar :: Shell -> (Text,Text) -> Text
+exportEnvar Fish (var,value) = "set -x "<>var<>space<>doubleQuote value
+exportEnvar _    (var,value) = "export "<>var<>"="<>doubleQuote value
+
+updatePath :: Shell -> Text -> Text
+updatePath Fish dir = "fish_add_path "<>doubleQuote dir
+updatePath sh   dir = exportEnvar sh ("PATH",doubleQuote $ dir<>colon<>"$PATH")
+
+sourceScript :: Text -> Text
+sourceScript script = "source " <> script
+
+updateZshFPath :: FilePath -> Text
+updateZshFPath (DT.pack->dir) = "fpath=("<>doubleQuote dir<>" $fpath)"
+
+compinitZsh :: Text
+compinitZsh = "autoload -Uz compinit && compinit"
+
+completionsDirPath :: FilePath -> BKConfigPaths -> Shell -> FilePath
+completionsDirPath homedir _ Fish = 
+      homedir 
+  </> ".config" 
+  </> "fish" 
+  </> "completions" 
+completionsDirPath _ configPaths _ = 
+  let configDir = bkConfigDirectoryPath configPaths       
+   in configDir </> "completions"
+
+initShellScript :: FilePath -> BKConfigPaths -> Shell -> Text
+initShellScript homedir configPaths = _initShellScript
+  where
+    configDir = DT.pack $ bkConfigDirectoryPath configPaths     
+
+    configDirEnvar :: (Text,Text)
+    configDirEnvar = ("BK_CONFIG_DIR", configDir)
+
+    _initShellScript :: Shell -> Text
+    _initShellScript sh@Bash = exportEnvar sh configDirEnvar <> newline
+                            <> sourceScript (DT.pack $ completionsDirPath homedir configPaths sh </> "_bk.sh")
+    _initShellScript sh@Zsh  = exportEnvar sh configDirEnvar <> newline
+                            <> updateZshFPath (completionsDirPath homedir configPaths sh) <> newline
+                            <> compinitZsh
+    _initShellScript _ = undefined
+
+initShell :: Shell -> BKConfigPaths -> IO ()
+initShell shell configPaths = undefined
